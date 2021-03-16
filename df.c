07264fa025c12c7b9feb1940f26aec6fc9384824
chaquator <chaquator@gmail.com>
2021-03-16 03:09:27 -0500

    added testing function not my best work

--- df.c
+++ df.c
@@ -22,7 +22,7 @@ float parabola_intersect(struct view_f f, size_t p, size_t q) {
 // f -- single row buffer of parabola heights, sized N
 // v -- vertices buffer, sized N
 // z -- intersections buffer, sized N+1
-void dist_transform_1d(struct view_f f, struct view_st v, struct view_f z) {
+void dist_transform_1d(struct view_f f, struct view_st v, struct view_f z, struct test_results* test) {
     assert((f.end - f.start) > 0);
     assert((v.end - v.start) > 0);
     assert((z.end - z.start) > 0);
@@ -34,18 +34,19 @@ void dist_transform_1d(struct view_f f, struct view_st v, struct view_f z) {
 
     // Part 1: Compute lower envelope as a set of break points and vertices
     // Start at the first non-infinity parabola
-    ptrdiff_t offset = 0;
-    while (isinf(f.start[offset]) && offset < (f.end - f.start)) ++offset;
+    size_t offset = 0;
+    while (isinf(f.start[offset]) && offset < (size_t)(f.end - f.start)) ++offset;
 
     // If lower envelope is all at infinity, we have an empty row, this is complete as far as we care
-    if (offset == (f.end - f.start)) return;
+    if (offset == (size_t)(f.end - f.start)) return;
 
     v.start[0] = offset;
     z.start[0] = -INFINITY;
     z.start[1] = INFINITY;
 
+    // TODO: maybe consider chanigng this to size_t once finished testing
     ptrdiff_t k = 0;
-    for (ptrdiff_t q = offset + 1; q < (f.end - f.start); ++q) {
+    for (size_t q = offset + 1; q < (size_t)(f.end - f.start); ++q) {
         if (isinf(f.start[q])) continue;
         // Calculate intersection of current parabola and next candidate
         float s = parabola_intersect(f, v.start[k], q);
@@ -67,11 +68,15 @@ void dist_transform_1d(struct view_f f, struct view_st v, struct view_f z) {
     size_t j = 0;
     for (ptrdiff_t q = 0; q < (f.end - f.start); ++q) {
         // Seek break-point past q
-        while (z.start[j + 1] < q) ++j;
+        while (z.start[j + 1] < (float)q) ++j;
         // Set point at f to parabola (originating at v[j])
         size_t v_k = v.start[j];
-        f.start[q] = (q - v_k) * (q - v_k) + f.start[v_k];
+        float displacement = (float)q - (float)v_k;
+        f.start[q] = displacement * displacement + f.start[v_k];
     }
+
+    test->k = k;
+    test->j = j;
 }
 
 // TODO: optimize out the N+1 to N
--- df.h
+++ df.h
@@ -5,8 +5,14 @@
 
 #include <stddef.h>
 
+// results to get back from test
+struct test_results {
+    ptrdiff_t k;
+    size_t j;
+};
+
 float parabola_intersect(struct view_f f, size_t p, size_t q);
 
-void dist_transform_1d(struct view_f f, struct view_st v, struct view_f z);
+void dist_transform_1d(struct view_f f, struct view_st v, struct view_f z, struct test_results* test);
 
 #endif
