36a971cd5106d97f5570cf229ab10107495d413c
chaquator <chaquator@gmail.com>
2021-03-16 02:01:18 -0500

    1-d distance transform is almost tested need to optimize and write permanent test, then optimize

--- df.c
+++ df.c
@@ -3,14 +3,13 @@
 #include <assert.h>
 #include <math.h>
 
-#define BIG INFINITY
-
 // intersection of 2 parabolas, not defined if both parabolas have vertex y's at infinity
 float parabola_intersect(struct view_f f, size_t p, size_t q) {
-    assert(p >= 0);
-    assert(q > 0);
-    assert(p < (f.end - f.start));
-    assert(q < (f.end - f.start));
+    assert(p != q);
+    assert((ptrdiff_t)p < (f.end - f.start));
+    assert((ptrdiff_t)q < (f.end - f.start));
+    assert(!isinf(f.start[p]));
+    assert(!isinf(f.start[q]));
 
     float fp = (float)p;
     float fq = (float)q;
@@ -30,38 +29,51 @@ void dist_transform_1d(struct view_f f, struct view_st v, struct view_f z) {
     assert((v.end - v.start) == (f.end - f.start));
     assert((z.end - z.start) == (f.end - f.start) + 1);
 
-    // Part 1: compute lower envelope as a set of break points and vertices
-    v.start[0] = 0.f;
-    z.start[0] = -BIG;
-    z.start[1] = BIG;
+    // Single-cell is already complete
+    if ((f.end - f.start) == 1) return;
+
+    // Part 1: Compute lower envelope as a set of break points and vertices
+    // Start at the first non-infinity parabola
+    ptrdiff_t offset = 0;
+    while (isinf(f.start[offset]) && offset < (f.end - f.start)) ++offset;
+
+    // If lower envelope is all at infinity, we have an empty row, this is complete as far as we care
+    if (offset == (f.end - f.start)) return;
+
+    v.start[0] = offset;
+    z.start[0] = -INFINITY;
+    z.start[1] = INFINITY;
+
     ptrdiff_t k = 0;
-    for (size_t q = 1; q < (f.end - f.start); ++q) {
+    for (ptrdiff_t q = offset + 1; q < (f.end - f.start); ++q) {
+        if (isinf(f.start[q])) continue;
         // Calculate intersection of current parabola and next candidate
         float s = parabola_intersect(f, v.start[k], q);
 
         // If this intersection comes before current left-bound, we must back up and change the necessary break point
         while (s <= z.start[k]) {
             --k;
-            assert(k > 0);
+            assert(k >= 0);
             s = parabola_intersect(f, v.start[k], q);
         }
         // Once we found a suitable break point, update the structure
         ++k;
         v.start[k] = q;
         z.start[k] = s;
-        z.start[k + 1] = BIG;
+        z.start[k + 1] = INFINITY;
     }
 
-    // Part 2: populate f using lower envelope
-    k = 0;
-    for (size_t q = 0; q < (f.end - f.start); ++q) {
+    // Part 2: Populate f using lower envelope
+    size_t j = 0;
+    for (ptrdiff_t q = 0; q < (f.end - f.start); ++q) {
         // Seek break-point past q
-        while (z.start[k + 1] < q)
-            ++k;
-        // Set point at f to parabola (originating at v[k])
-        size_t v_k = v.start[k];
+        while (z.start[j + 1] < q) ++j;
+        // Set point at f to parabola (originating at v[j])
+        size_t v_k = v.start[j];
         f.start[q] = (q - v_k) * (q - v_k) + f.start[v_k];
     }
 }
 
-// TODO: test for parabolas at infinity (1(q) indicator)
+// TODO: optimize out the N+1 to N
+// Not only does the highest right bound being set to infinity not matter if you keep track of the # of hulls
+// But I don't think the lowest left bound being -infinity is necessary either if you move some stuff out
--- sdftest.c
+++ sdftest.c
@@ -1,12 +1,15 @@
+#include <math.h>
 #include <stdio.h>
 
 #include "df.h"
 #include "view.h"
 
+// TODO: write testing function for various tests
+
 int main() {
-    float f[] = {7.1f, 5.9f, 4.f, 2.4f, 0.f};
-    size_t v[sizeof(f) / sizeof(float)] = {};
-    float z[sizeof(f) / sizeof(float) + 1] = {};
+    float f[] = {0, 1, 2, 3, 4};
+    size_t v[sizeof(f) / sizeof(float)] = {0};
+    float z[sizeof(f) / sizeof(float) + 1] = {0};
     dist_transform_1d((struct view_f){f, f + sizeof(f) / sizeof(float)},
                       (struct view_st){v, v + sizeof(v) / sizeof(size_t)},
                       (struct view_f){z, z + sizeof(z) / sizeof(float)});
