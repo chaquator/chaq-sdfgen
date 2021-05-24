93c9189d5a96d7b0aae6b3e020ab14dc24033126
chaquator <chaquator@gmail.com>
2021-05-24 01:42:55 -0500

    changed flow of tasks for performance increase instead of: df 1st pass --> transpose --> df 2nd pass --> transpose & sqrt it is now: df & transpose --> df & transpose

--- df.c
+++ df.c
@@ -1,78 +1,103 @@
 #include "df.h"
 #include <assert.h>
 #include <math.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
 // intersection of 2 parabolas, not defined if both parabolas have vertex y's at infinity
-static float parabola_intersect(struct view_f f, size_t p, size_t q) {
+static float parabola_intersect(float* restrict f, size_t p, size_t q) {
     float fp = (float)p;
     float fq = (float)q;
-    return ((f.start[q] - f.start[p]) + ((fq * fq) - (fp * fp))) / (2 * (fq - fp));
+    return ((f[q] - f[p]) + ((fq * fq) - (fp * fp))) / (2 * (fq - fp));
 }
 
 // Compute euclidean distance transform in 1d using passed in buffers
 // Reference: Distance Transforms of Sampled Functions (P. Felzenszwalb, D. Huttenlocher):
 //      http://cs.brown.edu/people/pfelzens/dt/
-// f -- single row buffer of parabola heights, sized N
-// v -- vertices buffer, sized N
-// h -- vertex height buffer, sized N
-// z -- break point buffer, associates z[n] with v[n]'s right bound, sized N-1
-static void dist_transform_1d(struct view_f f, struct view_st v, struct view_f h, struct view_f z) {
+// img_row -- single row buffer of parabola heights
+// w -- size of img_row
+// y -- current y for transpose
+// v -- vertices buffer, sized w
+// h -- vertex height buffer, sized w
+// z -- break point buffer, associates z[n] with v[n]'s right bound, sized w-1
+// img_tpose_out -- output buffer for distance transform, will be populated in transpose, must be sufficiently large
+// (at minimum w * y)
+// do_sqrt -- whether to compute sqrt of value after computing lower envelope
+static void dist_transform_1d(float* restrict img_row, size_t w, size_t y, size_t* restrict v, float* restrict h,
+                              float* restrict z, float* restrict img_tpose_out, bool do_sqrt) {
     // Single-cell is already complete
-    if ((f.end - f.start) <= 1) return;
+    if (w <= 1) {
+        // Write back to single cell
+        img_tpose_out[0] = img_row[0];
+        return;
+    }
 
     // Part 1: Compute lower envelope as a set of break points and vertices
     // Start at the first non-infinity parabola
     size_t offset = 0;
-    while (isinf(f.start[offset]) && offset < (size_t)(f.end - f.start)) ++offset;
+    while (isinf(img_row[offset]) && offset < w) ++offset;
 
     // If lower envelope is all at infinity, we have an empty row, this is complete as far as we care
-    if (offset == (size_t)(f.end - f.start)) return;
+    if (offset == w) {
+        // Because we're transposing on writeback, we need to fill empty rows
+        for (size_t i = 0; i < w; ++i) {
+            size_t tpose_idx = y + w * i;
+            img_tpose_out[tpose_idx] = INFINITY;
+        }
+        return;
+    }
 
     // First vertex is that of the first parabola
-    v.start[0] = offset;
-    h.start[0] = f.start[offset];
+    v[0] = offset;
+    h[0] = img_row[offset];
 
     size_t k = 0;
-    for (size_t q = offset + 1; q < (size_t)(f.end - f.start); ++q) {
+    for (size_t q = offset + 1; q < w; ++q) {
         // Skip parabolas at infinite heights (essentially non-existant parabolas)
-        if (isinf(f.start[q])) continue;
+        if (isinf(img_row[q])) continue;
 
         // Calculate intersection of current parabola and next candidate
-        float s = parabola_intersect(f, v.start[k], q);
+        float s = parabola_intersect(img_row, v[k], q);
 
         // If this intersection comes before current left bound, we must back up and change the necessary break point
         // Skip for k == 0 because there is no left bound to look back on (it is at -infinity)
-        while (k > 0 && s <= z.start[k - 1]) {
+        while (k > 0 && s <= z[k - 1]) {
             --k;
-            s = parabola_intersect(f, v.start[k], q);
+            s = parabola_intersect(img_row, v[k], q);
         }
         // Once we found a suitable break point, update the structure
         // Right bound of current parabola is intersection
-        z.start[k] = s;
+        z[k] = s;
         ++k;
         // Horizontal position of next parabola is vertex
-        v.start[k] = q;
+        v[k] = q;
         // Vertical position of next parabola
-        h.start[k] = f.start[q];
+        h[k] = img_row[q];
     }
 
-    // Part 2: Populate f using lower envelope
+    // Part 2: Populate img_row using lower envelope
     size_t j = 0;
-    for (ptrdiff_t q = 0; q < (f.end - f.start); ++q) {
+    for (size_t q = 0; q < w; ++q) {
         // Seek break point past q
-        while (j < k && z.start[j] < (float)q) ++j;
-        // Set point at f to parabola (originating at v[j])
-        size_t v_j = v.start[j];
+        while (j < k && z[j] < (float)q) ++j;
+
+        // Set height at current position (q) along output to lower envelope
+        size_t v_j = v[j];
         float displacement = (float)q - (float)v_j;
-        f.start[q] = displacement * displacement + h.start[j];
+
+        // Output transposed
+        size_t tpose_idx = y + w * q;
+        img_tpose_out[tpose_idx] = displacement * displacement + h[j];
+
+        if (do_sqrt) img_tpose_out[tpose_idx] = sqrtf(img_tpose_out[tpose_idx]);
     }
 }
 
-// compute distance transform along x-axis of image using buffers passed in
+// Compute distance transform along x-axis of image using buffers passed in
 // img must be at least w*h floats large
-static void dist_transform_axis(float* img, size_t w, size_t h) {
+// Writes back to img_out in transpose which must be h*w floats large
+static void dist_transform_axis(float* restrict img, size_t w, size_t h, float* restrict img_tpose_out, bool do_sqrt) {
 #pragma omp parallel
     {
         ptrdiff_t y;
@@ -83,16 +108,10 @@ static void dist_transform_axis(float* img, size_t w, size_t h) {
         // Break point buffer
         float* z = malloc(sizeof(float) * (size_t)(w - 1));
 
-        struct view_st v_v = {.start = v, .end = v + w};
-        struct view_f v_p = {.start = p, .end = p + w};
-        struct view_f v_z = {.start = z, .end = z + (w - 1)};
-
 #pragma omp for schedule(static)
         for (y = 0; y < (ptrdiff_t)(h); ++y) {
-            // partition img, z, and v into views and pass into dist transform
-            struct view_f f = {.start = img + ((size_t)y * w), .end = img + (((size_t)y + 1) * w)};
-
-            dist_transform_1d(f, v_v, v_p, v_z);
+            float* img_slice = img + ((size_t)y * w);
+            dist_transform_1d(img_slice, w, (size_t)y, v, p, z, img_tpose_out, do_sqrt);
         }
 
         free(z);
@@ -101,42 +120,15 @@ static void dist_transform_axis(float* img, size_t w, size_t h) {
     }
 }
 
-// copy transpose of src to dest, creates a dest that is h x w, where src is w x h
-// w -- width dimension of src
-// h -- height dimension of src
-static void transpose_cpy(float* restrict dest, float* restrict src, size_t w, size_t h) {
-    ptrdiff_t i;
-#pragma omp parallel for schedule(static)
-    for (i = 0; i < (ptrdiff_t)(w * h); ++i) {
-        size_t x = (size_t)i % w;
-        size_t y = (size_t)i / w;
-        dest[h * x + y] = src[(size_t)i];
-    }
-}
-
-static void transpose_cpy_sqrt(float* restrict dest, float* restrict src, size_t w, size_t h) {
-    ptrdiff_t i;
-#pragma omp parallel for schedule(static)
-    for (i = 0; i < (ptrdiff_t)(w * h); ++i) {
-        size_t x = (size_t)i % w;
-        size_t y = (size_t)i / w;
-        dest[h * x + y] = sqrtf(src[(size_t)i]);
-    }
-}
-
 void dist_transform_2d(float* img, size_t w, size_t h) {
     // compute 1d for all rows
-    dist_transform_axis(img, w, h);
-
-    // transpose
     float* img_tpose = malloc(w * h * sizeof(float));
-    transpose_cpy(img_tpose, img, w, h);
 
-    // compute 1d for all rows (now for all columns)
-    dist_transform_axis(img_tpose, h, w);
+    // compute distance transform and store transposed into img_tpose
+    dist_transform_axis(img, w, h, img_tpose, false);
 
-    // tranpose back while computing square root
-    transpose_cpy_sqrt(img, img_tpose, h, w);
+    // now do pass on transpose and store back into original image
+    dist_transform_axis(img_tpose, h, w, img, true);
 
     free(img_tpose);
 }
--- df.h
+++ df.h
@@ -1,8 +1,6 @@
 #ifndef DF_H
 #define DF_H
 
-#include "view.h"
-
 #include <stddef.h>
 
 void dist_transform_2d(float* img, size_t w, size_t h);
