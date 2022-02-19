d4ef6f07d40e5c27887b037ae429354664a77364
chaquator <chaquator@gmail.com>
2022-02-18 18:03:32 -0600

    vk validation layer

--- vulkan/main.cpp
+++ vulkan/main.cpp
@@ -23,9 +23,68 @@ static vk::Queue queue;
 static vk::CommandPool cmd_pool;
 static vk::CommandBuffer cmd_buffer;
 
+#ifndef NDEBUG
+
+static vk::DispatchLoaderDynamic dynamic_dispatch;
+static vk::DebugUtilsMessengerEXT debug_messenger;
+
+static VKAPI_ATTR vk::Bool32 VKAPI_CALL debug_cb(VkDebugUtilsMessageSeverityFlagBitsEXT severity,
+                                                 VkDebugUtilsMessageTypeFlagsEXT type,
+                                                 const VkDebugUtilsMessengerCallbackDataEXT* callback_data,
+                                                 void* user_data) {
+
+    (void)user_data;
+
+    const auto msg_severity = vk::DebugUtilsMessageSeverityFlagBitsEXT(severity);
+    const auto msg_type = vk::DebugUtilsMessageSeverityFlagBitsEXT(type);
+
+    constexpr const auto debug_msg = "Vk Validation Layer (Type: {}): {}";
+    const auto type_str = vk::to_string(msg_type);
+
+    switch (msg_severity) {
+    case vk::DebugUtilsMessageSeverityFlagBitsEXT::eError:
+        spdlog::error(debug_msg, type_str, callback_data->pMessage);
+        break;
+    case vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo:
+        spdlog::info(debug_msg, type_str, callback_data->pMessage);
+        break;
+    case vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose:
+        spdlog::trace(debug_msg, type_str, callback_data->pMessage);
+        break;
+    case vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning:
+        spdlog::warn(debug_msg, type_str, callback_data->pMessage);
+        break;
+    }
+
+    return VK_FALSE;
+}
+
+static bool init_debug_messenger(vk::Instance& instance) {
+    using severity = vk::DebugUtilsMessageSeverityFlagBitsEXT;
+    using msg_type = vk::DebugUtilsMessageTypeFlagBitsEXT;
+    auto sev_flags = severity::eVerbose | severity::eError | severity::eWarning;
+    auto msg_flags = msg_type::eGeneral | msg_type::ePerformance | msg_type::eValidation;
+    const vk::DebugUtilsMessengerCreateInfoEXT create_info(vk::DebugUtilsMessengerCreateFlagBitsEXT{}, sev_flags,
+                                                           msg_flags, (PFN_vkDebugUtilsMessengerCallbackEXT)debug_cb,
+                                                           nullptr);
+
+    dynamic_dispatch = vk::DispatchLoaderDynamic(instance, vkGetInstanceProcAddr);
+    const auto messenger_opt = instance.createDebugUtilsMessengerEXT(create_info, nullptr, dynamic_dispatch);
+    if (messenger_opt.result != vk::Result::eSuccess) {
+        spdlog::error("Failed to create Vk debug messenger! (VkResult: {})", vk::to_string(messenger_opt.result));
+        return false;
+    }
+
+    debug_messenger = messenger_opt.value;
+
+    return true;
+}
+
+#endif
+
 template <class It>
-std::optional<std::tuple<const vk::PhysicalDevice, std::size_t>> get_suitable_device_and_queue_family_idx(It begin,
-                                                                                                          It end) {
+static std::optional<std::tuple<const vk::PhysicalDevice, std::size_t>>
+get_suitable_device_and_queue_family_idx(It begin, It end) {
     auto start = begin;
     while (start != end) {
         const auto& device = *start;
@@ -46,11 +105,19 @@ std::optional<std::tuple<const vk::PhysicalDevice, std::size_t>> get_suitable_de
     return {};
 }
 
-bool init_vk() {
-    spdlog::trace("Creating VkInstance");
+static bool init_vk() {
+    spdlog::debug("Creating VkInstance");
+
+#ifndef NDEBUG
+    const auto layers = {"VK_LAYER_KHRONOS_validation"};
+    const auto extensions = {VK_EXT_DEBUG_UTILS_EXTENSION_NAME};
+#else
+    const auto layers = {};
+    const auto extensions = {};
+#endif
 
     vk::ApplicationInfo app_info(app_name, 1, nullptr, 0);
-    vk::InstanceCreateInfo inst_info({}, &app_info);
+    vk::InstanceCreateInfo inst_info({}, &app_info, layers, extensions);
 
     const auto instance_opt = vk::createInstance(inst_info);
 
@@ -64,8 +131,8 @@ bool init_vk() {
     return true;
 }
 
-bool init_logical_device() {
-    spdlog::trace("Getting VkPhysicalDevice list");
+static bool init_logical_device() {
+    spdlog::debug("Getting VkPhysicalDevice list");
 
     const auto physical_devices_opt = instance.enumeratePhysicalDevices();
 
@@ -77,7 +144,7 @@ bool init_logical_device() {
 
     const auto& physical_devices = physical_devices_opt.value;
 
-    spdlog::trace("Searching for suitable VkPhysicalDevice");
+    spdlog::debug("Searching for suitable VkPhysicalDevice");
 
     const auto device_queue_opt =
         get_suitable_device_and_queue_family_idx(physical_devices.cbegin(), physical_devices.cend());
@@ -91,9 +158,9 @@ bool init_logical_device() {
     std::tie(physical_device, queue_family_idx) = *device_queue_opt;
 
     spdlog::info("Physical device: {}", physical_device.getProperties().deviceName.data());
-    spdlog::trace("Queue family index: {}", queue_family_idx);
+    spdlog::debug("Queue family index: {}", queue_family_idx);
 
-    spdlog::trace("Creating logical VkDevice");
+    spdlog::debug("Creating logical VkDevice");
 
     const auto queue_priorities = {0.f};
     const auto queue_create_info = {vk::DeviceQueueCreateInfo({}, queue_family_idx, queue_priorities)};
@@ -111,8 +178,8 @@ bool init_logical_device() {
     return true;
 }
 
-bool init_command_pool() {
-    spdlog::trace("Creating VkCommandPool");
+static bool init_command_pool() {
+    spdlog::debug("Creating VkCommandPool");
 
     const auto cmd_pool_opt = device.createCommandPool(
         vk::CommandPoolCreateInfo(vk::CommandPoolCreateFlagBits::eResetCommandBuffer, queue_family_idx));
@@ -127,8 +194,8 @@ bool init_command_pool() {
     return true;
 }
 
-bool init_command_buffer() {
-    spdlog::trace("Creating main VkCommandBuffer");
+static bool init_command_buffer() {
+    spdlog::debug("Creating main VkCommandBuffer");
 
     const auto cmd_buffer_opt =
         device.allocateCommandBuffers(vk::CommandBufferAllocateInfo(cmd_pool, vk::CommandBufferLevel::ePrimary, 1));
@@ -144,8 +211,12 @@ bool init_command_buffer() {
     return true;
 }
 
-void destroy_vk() {
-    spdlog::trace("Cleaning up resources");
+static void destroy_vk() {
+    spdlog::debug("Cleaning up resources");
+
+#ifndef NDEBUG
+    instance.destroyDebugUtilsMessengerEXT(debug_messenger, nullptr, dynamic_dispatch);
+#endif
 
     device.freeCommandBuffers(cmd_pool, {cmd_buffer});
     device.destroyCommandPool(cmd_pool);
@@ -154,12 +225,19 @@ void destroy_vk() {
 }
 
 int main() {
-    spdlog::set_level(spdlog::level::trace);
+    spdlog::set_level(spdlog::level::debug);
 
     if (!init_vk()) {
         spdlog::critical("Failed to init Vulkan");
         return -1;
     };
+
+#ifndef NDEBUG
+    if (!init_debug_messenger(instance)) {
+        spdlog::critical("Failed to init debug messenger");
+        return -1;
+    }
+#endif
 
     if (!init_logical_device()) {
         spdlog::critical("Failed to init VkDevice");
